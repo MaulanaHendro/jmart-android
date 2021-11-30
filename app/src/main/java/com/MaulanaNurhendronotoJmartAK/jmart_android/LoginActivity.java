53e165b70f378732b9de29abbf6f42600cec9de6
“[Maulana <[maulana.nurhendronoto@ui.ac.id]>
2021-11-30 21:57:57 +0700

    Revisi CS Modul 9

--- app/src/main/java/com/MaulanaNurhendronotoJmartAK/jmart_android/LoginActivity.java
+++ app/src/main/java/com/MaulanaNurhendronotoJmartAK/jmart_android/LoginActivity.java
@@ -6,23 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-import com.android.volley.Request;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.MaulanaNurhendronotoJmartAK.jmart_android.model.Account;
 import com.MaulanaNurhendronotoJmartAK.jmart_android.request.LoginRequest;
-import android.os.Bundle;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
+
     private static final Gson gson = new Gson();
     private static Account loggedAccount = null;
 
-    public static Account getLoggedAccount(){
+    public static Account getLoggedAccount()
+    {
         return loggedAccount;
     }
 
@@ -31,27 +34,43 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText textemail = findViewById(R.id.emailActivityLayout);
-        EditText textpassword = findViewById(R.id.passwordLoginActivity);
-        Button buttonlogin = findViewById(R.id.buttonLoginActivity);
-        buttonlogin.setOnClickListener(new View.OnClickListener() {
+       final EditText textemail= findViewById(R.id.emailLoginActivity);
+       final EditText textpassword = findViewById(R.id.passwordLoginActivity);
+       final Button  buttonlogin = findViewById(R.id.buttonLoginActivity);
+       final TextView  registerlogin = findViewById(R.id.registerLoginActivity);
+
+        registerlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonlogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public  void onClick (View v)
+            {
                 Response.Listener<String> listener = new Response.Listener<String>() {
                     @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject object = new JSONObject(response);
-                            if(object != null){
-                                Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT);
+                    public void onResponse(String response)
+                    {
+                        try
+                        {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject!=null)
+                            {
+                                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                loggedAccount = gson.fromJson(object.toString(), Account.class);
+                                loggedAccount = gson.fromJson(jsonObject.toString(),Account.class);
                                 startActivity(intent);
                             }
                         }
-                        catch (JSONException e){
+                        catch (JSONException e)
+                        {
                             e.printStackTrace();
-                            Toast.makeText(LoginActivity.this, "Login Error!", Toast.LENGTH_SHORT);
+                            Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                         }
                     }
                 };
--- app/src/main/java/com/MaulanaNurhendronotoJmartAK/jmart_android/RegisterActivity.java
+++ app/src/main/java/com/MaulanaNurhendronotoJmartAK/jmart_android/RegisterActivity.java
@@ -2,13 +2,64 @@ package com.MaulanaNurhendronotoJmartAK.jmart_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.MaulanaNurhendronotoJmartAK.jmart_android.model.Account;
+import com.MaulanaNurhendronotoJmartAK.jmart_android.request.RegisterRequest;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final Gson gson = new Gson();
+    private static Account loggedAccount = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+       final EditText nameregister = findViewById(R.id.nameRegisterActivity);
+       final EditText emailregister = findViewById(R.id.emailRegisterActivity);
+       final EditText passwordregister = findViewById(R.id.passwordRegisterActivity);
+       final Button  registerbutton = findViewById(R.id.buttonRegisterActivity);
+
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Response.Listener<String> listener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject!=null)
+                            {
+                                Toast.makeText(RegisterActivity.this, "Registration success", Toast.LENGTH_SHORT);
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                loggedAccount = gson.fromJson(jsonObject.toString(),Account.class);
+                                startActivity(intent);
+                            }
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                            Toast.makeText(RegisterActivity.this, "registration failed", Toast.LENGTH_SHORT);
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(nameregister.getText().toString(), emailregister.getText().toString(), passwordregister.getText().toString(), listener, null);
+                RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+                requestQueue.add(registerRequest);
+            }
+        });
     }
 }
